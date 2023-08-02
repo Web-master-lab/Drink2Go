1a805d26e42f4743f29203640e9355fd98b8e003
Gleb <stepanov-gleb2003@mail.ru>
2023-08-02 22:07:56 +0300

    Доделал мобильные стили и сделал планшетные.

--- source/js/script.js
+++ source/js/script.js
@@ -11,3 +11,33 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.promo__button-control--prev',
   },
 });
+
+const map = L.map('map')
+  .setView({
+    lat: 59.968516,
+    lng: 30.317768
+  }, 20);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },)
+  .addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: '../img/map-pin.svg',
+    iconSize: [38, 50],
+    iconAnchor: [19, 50]
+  });
+
+  const mainPinMarker = L.marker(
+    {
+      lat: 59.968516,
+      lng: 30.317768
+    },
+    {
+      draggable: false,
+      icon: mainPinIcon
+    })
+    .addTo(map);
